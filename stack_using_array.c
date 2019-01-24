2fc3b8498d9a76fb647b9be0a86e4b86404e1c8e
Junsoo <junsoo.feb@gmail.com>
2019-01-24 23:02:05 +0900

    initial commit

---
+++ stack_using_array.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int stact_array[100];//array of size 100
+int t = 0; // t  == (index of stack_top) + 1
+
+int pop()
+{
+	if (t < 1)
+	{
+		printf("Stack is empty!\n\n");
+		return -1;
+	}
+
+	t = t - 1;
+	return stact_array[t];
+}
+
+void push(int x)
+{
+	if (t >= 100)
+	{
+		printf("Stack is full!\n\n");
+		return;
+	}
+
+	stact_array[t] = x;
+	t = t + 1;
+}
+
+void print_array()
+{
+	int i;
+	for (i = 0; i < t; i++)
+	{
+		printf("stack index : %d     item : %c\n", i, stact_array[i]);
+	}
+}
+
+int main()
+{
+	pop();
+	push('a');
+	push('b');
+	push('c');
+	print_array();
+
+	return;
+}
